fd4e8b2e25ae15b05c22b00892b2f9d3d07eb530
Biswajit Dash <51367724+biswat@users.noreply.github.com>
2021-07-01 04:19:11 -0700

    Update urls.py

--- python_work/urls.py
+++ python_work/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from python_work.python_work import main
+#from python_work.python_work import main
 from django.contrib import admin
 from django.urls import path
 from django.views import static
@@ -32,4 +32,4 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
-urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
